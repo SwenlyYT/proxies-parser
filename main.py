3284a51fda9075c271a75a4b18e50af54c88162c
SwenlyYT <93844393+SwenlyYT@users.noreply.github.com>
2022-05-02 14:32:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,43 +11,58 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'
 }
 
-r = requests.get(f'https://hidemy.name/ru/proxy-list/?maxtime=100#list', headers=HEADERS)
-soup = BS(r.content, 'html.parser')
-
 try:
-    f = open('proxies.txt', 'r')
-    f_read = f.read()
+    f = open('http.txt', 'r')
+    h_read = f.read()
     f.close()
 except:
-    open('proxies.txt', 'w').write('')
-    f = open('proxies.txt', 'r')
-    f_read = f.read()
+    open('http.txt', 'w').write('')
+    f = open('http.txt', 'r')
+    h_read = f.read()
+    f.close()
+try:
+    f = open('socks4.txt', 'r')
+    s4_read = f.read()
+    f.close()
+except:
+    open('socks4.txt', 'w').write('')
+    f = open('socks4.txt', 'r')
+    s4_read = f.read()
+    f.close()
+try:
+    f = open('socks5.txt', 'r')
+    s5_read = f.read()
+    f.close()
+except:
+    open('socks5.txt', 'w').write('')
+    f = open('socks5.txt', 'r')
+    s5_read = f.read()
     f.close()
 
-proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
-
-lol = 0
 
 sleep(3)
 print('Парсю прокси...')
-
-with open('proxies.txt', 'a') as file:
+with open('socks4.txt', 'a') as file:
+    lol = 0
+    r = requests.get(f'https://hidemy.name/ru/proxy-list/?type=4#list', headers=HEADERS)
+    soup = BS(r.content, 'html.parser')
+    proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
     for i in proxies:
         _proxy = i.find('td')
         _port = _proxy.find_next('td').text
         _proxy = _proxy.text
         proxy = _proxy + ':' + _port
-        if proxy in f_read: continue
+        if proxy in s4_read: continue
         if lol == 0:
-            if f_read == [''] or f_read == [] or f_read == '':
+            if s4_read == [''] or s4_read == [] or s4_read == '':
                 file.write(proxy)
-                lol = 1
+            lol = 1
         else:
             file.write(f'\n' + proxy)
 
 while True:
     try:
-        with open('proxies.txt', 'a') as file:
+        with open('socks4.txt', 'a') as file:
             site = soup.find('li', class_='next_array').find('a').get('href')
 
             r = requests.get(f'https://hidemy.name{site}', headers=HEADERS)
@@ -60,11 +75,86 @@ while True:
                 _port = _proxy.find_next('td').text
                 _proxy = _proxy.text
                 proxy = _proxy + ':' + _port
-                if proxy in f_read: continue
+                if proxy in s4_read: continue
                 file.write(f'\n' + proxy)
-            
 
     except Exception as e:
-        print(e)
         break
 
+with open('socks5.txt', 'a') as file:
+    lol = 0
+    r = requests.get(f'https://hidemy.name/ru/proxy-list/?type=5#list', headers=HEADERS)
+    soup = BS(r.content, 'html.parser')
+    proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
+    for i in proxies:
+        _proxy = i.find('td')
+        _port = _proxy.find_next('td').text
+        _proxy = _proxy.text
+        proxy = _proxy + ':' + _port
+        if proxy in s5_read: continue
+        if lol == 0:
+            if s5_read == [''] or s5_read == [] or s5_read == '':
+                file.write(proxy)
+            lol = 1
+        else:
+            file.write(f'\n' + proxy)
+
+while True:
+    try:
+        with open('socks5.txt', 'a') as file:
+            site = soup.find('li', class_='next_array').find('a').get('href')
+
+            r = requests.get(f'https://hidemy.name{site}', headers=HEADERS)
+            soup = BS(r.content, 'html.parser')
+
+            proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
+
+            for i in proxies:
+                _proxy = i.find('td')
+                _port = _proxy.find_next('td').text
+                _proxy = _proxy.text
+                proxy = _proxy + ':' + _port
+                if proxy in s5_read: continue
+                file.write(f'\n' + proxy)
+
+    except Exception as e:
+        break
+
+with open('http.txt', 'a') as file:
+    lol = 0
+    r = requests.get(f'https://hidemy.name/ru/proxy-list/?maxtime=100#list', headers=HEADERS)
+    soup = BS(r.content, 'html.parser')
+    proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
+    for i in proxies:
+        _proxy = i.find('td')
+        _port = _proxy.find_next('td').text
+        _proxy = _proxy.text
+        proxy = _proxy + ':' + _port
+        if proxy in h_read: continue
+        if lol == 0:
+            if h_read == [''] or h_read == [] or h_read == '':
+                file.write(proxy)
+                lol = 1
+        else:
+            file.write(f'\n' + proxy)
+
+while True:
+    try:
+        with open('http.txt', 'a') as file:
+            site = soup.find('li', class_='next_array').find('a').get('href')
+
+            r = requests.get(f'https://hidemy.name{site}', headers=HEADERS)
+            soup = BS(r.content, 'html.parser')
+
+            proxies = soup.find('div', class_='table_block').find('tbody').find_all('tr')
+
+            for i in proxies:
+                _proxy = i.find('td')
+                _port = _proxy.find_next('td').text
+                _proxy = _proxy.text
+                proxy = _proxy + ':' + _port
+                if proxy in h_read: continue
+                file.write(f'\n' + proxy)
+
+    except Exception as e:
+        break
